f630d3676a7020d8feec548dd438fafcc7f7074a
Adithyanmurthy <141011292+Adithyanmurthy@users.noreply.github.com>
2025-04-23 12:29:38 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
+def greet():
     return "Hello from Adithya Python inside Docker + Kubernetes!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9090) 
+    print(greet())
